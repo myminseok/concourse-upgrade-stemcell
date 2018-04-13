63e899d70cd551ac814e35eb65972bdef92d7a09
myminseok <myminseok@gmail.com>
2018-04-13 16:11:04 +0900

    test

---
+++ 1.clean.sh
@@ -0,0 +1,2 @@
+./fly -t tutorial login -c http://127.0.0.1:8080 
+./fly -t tutorial destroy-pipeline -p upgrade-stemcell
---
+++ 2.set.sh
@@ -0,0 +1,1 @@
+./fly -t tutorial set-pipeline -p upgrade-stemcell -c pipeline-apply.yml -l ../concourse-upgrade-stemcell-param.yml 
